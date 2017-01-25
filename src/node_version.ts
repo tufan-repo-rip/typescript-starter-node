6fb9c77cd6bbdd0735c5e14af140c7546ef257c6
Shishir <sramam@users.noreply.github.com>
2017-01-25 10:40:10 -0800

    for real this time...
    there is a reason, one shouldn't edit on the github interface...

--- src/node_version.ts
+++ src/node_version.ts
@@ -1,10 +1,10 @@
 
-import * as semver from 'semver';
-import * as pkg from '../package';
 
+import * as semver from 'semver';
+const pkg = require('../package');
 const {version} = process;
 
-export function check(desired) {
+export function check(desired = version) {
   // Run time validation of engine version
   if (!semver.satisfies(version, desired)) {
     const msg = `${pkg.name} requires node ${desired}. Current node version == ${version}. Aborting.`;
