26b64f6e55b64ddef934bb92aa9acf165d3a9237
sramam <sramam@users.noreply.github.com>
2017-03-12 07:13:30 -0700

    postinstall script tested and working. Do the CI gods agree

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -2,15 +2,20 @@
  * Born out of a frustration at the number of times an extra commit
  * is needed when the repo location changes. This is especially true
  * of such 'template' repos.
+ *
+ * Completely hacky and designed to work with git.
+ * Any usage beyond that is likely to result in pain.
+ * Large dosage Ibuprofen recommended.
+ *
  */
 const pkg = require('../package.json');
 const chalk = require('chalk');
-const gitConfig = require('git-config');
+const gitcfg = require('parse-git-config');
 const path = require('path');
 
-const config = gitConfig.sync();
+const config = gitcfg.keys(gitcfg.sync());
 const repo = {
-  name: path.parse(config.remote.url || '')
+  name: path.parse(config.remote.origin.url || '').name
 };
 
 if (pkg.name !== repo.name) {
@@ -20,10 +25,15 @@ if (pkg.name !== repo.name) {
     ));
 }
 
-if (pkg.repository.url != config.remote.url) {
+const url = {
+  pkg: pkg.repository.url.replace('git+', '').replace('+git', '').replace('.git', ''),
+  repo: config.remote.origin.url.replace('git+', '').replace('+git', '').replace('.git','')
+};
+
+if (url.pkg !== url.repo) {
   console.log(
     chalk.red.bold(
-      `Repository name mismatch. 'package.json:${pkg.repository.url}' !== 'git config: ${config.remote.url}'`
+      `Repository name mismatch. 'package.json:${pkg.repository.url}' !== 'git config: ${config.remote.origin.url}'`
     )
   )
 }
