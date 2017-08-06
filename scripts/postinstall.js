6908eb70e1d824bb5eb7f303575b775db01435c6
sramam <sramam@users.noreply.github.com>
2017-08-06 07:45:51 -0700

    check that code-of-conduct email has been modified from default. Print a warning if this remains to be done

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,12 +1,17 @@
 
 const repoUrl = require('./tasks/repo-url')
+const fs = require('fs');
 
 if (!__dirname.match(/.*node_modules.*/)) {
   repoUrl.validate();
-  console.log('\n');
-  console.log(`-----------------------------------------------`);
-  console.log(`  Please modify email address in ./conduct.md  `);
-  console.log(`-----------------------------------------------`);
-  console.log(`\n`);
+  const fname = 'code-of-conduct.md'
+  const conduct = fs.readFileSync(fname, 'utf8');
+  if (conduct.match(/\[REPLACE EMAIL\]/)) {
+    console.log(`\n`);
+    console.log(`-----------------------------------------------------`);
+    console.log(`  Please modify email address in ${fname}            `);
+    console.log(`-----------------------------------------------------`);
+    console.log(`\n`);
+  }
 }
 
